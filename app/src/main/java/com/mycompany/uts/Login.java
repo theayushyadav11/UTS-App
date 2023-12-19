2c794435178b0500c9d9110f7eea107e8e278d42
theayushyadav11 <143514610+theayushyadav11@users.noreply.github.com>
2023-12-20 02:57:19 +0530

    Update Login.java
    check

--- app/src/main/java/com/mycompany/uts/Login.java
+++ app/src/main/java/com/mycompany/uts/Login.java
@@ -1,16 +1,12 @@
 package com.mycompany.uts;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
 public class Login extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,16 +14,11 @@ public class Login extends AppCompatActivity {
     }
     public void  log(View v)
     {
-
         EditText text=findViewById(R.id.phone);
         SharedPreferences Preferences=getSharedPreferences("SourceStation", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor=Preferences.edit();
         editor.putString("phone", text.getText().toString());
         editor.apply();
-
-
-
-
         Intent i=new Intent(this, MainActivity.class);
         startActivity(i);
     }
